be41460ee7437c11c054057f16b998eb92eb0afd
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-05-26 15:27:15 -0300

    implementação do localStorage set e get

--- script.js
+++ script.js
@@ -9,29 +9,18 @@ const Modal = {
     }
 }
 
-const Transaction = {
-    all: [{
-            description: 'Luz',
-            amount: -50000,
-            date: '23/01/2021',
-        },
-        {
-            description: 'Website',
-            amount: 500000,
-            date: '23/01/2021',
-        },
-        {
-            description: 'Internet',
-            amount: -20000,
-            date: '23/01/2021',
-        },
-        {
-            description: 'App',
-            amount: 200000,
-            date: '23/01/2021',
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+    },
 
-        }
-    ],
+    set(transactions) {
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+    }
+}
+
+const Transaction = {
+    all: Storage.get(),
 
     add(transaction) {
         Transaction.all.push(transaction)
@@ -217,7 +206,10 @@ const App = {
         Transaction.all.forEach(DOM.addTransaction)
 
         DOM.updateBalance()
+
+        Storage.set(Transaction.all)
     },
+
     reload() {
         DOM.clearTransactions()
         App.init()
